34fe3998b1807d2ac723b44d4b5430e4b9affad8
dwatow <dwatow@gmail.com>
2014-10-19 21:00:07 +0800

    create CppunitLib.h
    將需要引用的檔案放在同一個.h檔中。

---
+++ MainProj/UnitTest/CppunitLib.h
@@ -0,0 +1,11 @@
+#ifndef CPPUNITLIB_H
+#define CPPUNITLIB_H
+
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/ui/text/Testrunner.h>
+#include <cppunit/XmlOutputter.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/CompilerOutputter.h>
+
+#endif
